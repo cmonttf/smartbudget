b2a2e23d3ccaa523b0587f596ac43ba3e0163c00
Camilo Montt <cmonttf@gmail.com>
2026-07-02 17:57:39 -0400

    feat: se agrega archivo js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('.menu__link');
+  links.forEach(link => link.addEventListener('mouseenter', () => link.classList.add('is-hover')));
+});
