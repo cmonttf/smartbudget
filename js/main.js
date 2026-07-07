d58813722dd9eb09beb98c5c5354debfaf49e3d3
Camilo Montt <cmonttf@gmail.com>
2026-07-07 12:29:51 -0400

    fix: se arregla nombre de clase

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const links = document.querySelectorAll('.menu__link');
+  const links = document.querySelectorAll('.menu-link');
   links.forEach(link => link.addEventListener('mouseenter', () => link.classList.add('is-hover')));
 });
